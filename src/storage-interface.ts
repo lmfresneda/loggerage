8e9bcac9c72ee448bedcbff6dfc8e18528808e29
Luis Miguel F <miguelfr.reg@gmail.com>
2017-08-11 18:10:39 +0200

    clean for doc

--- src/storage-interface.ts
+++
@@ -1,8 +0,0 @@
-import { LoggerageObject } from './loggerage-object';
-import { Query } from './utils/query';
-
-export interface Storage {
-  getItem(app:string, query?:Query): LoggerageObject[]|Promise<LoggerageObject[]>
-  setItem(app:string, value:LoggerageObject):void|Promise<void>
-  clear():void|Promise<void>
-}
--- types/wrap-localstorage.d.ts
+++
@@ -1,9 +0,0 @@
-import { LoggerageObject } from './loggerage-object';
-import { Storage } from './storage-interface';
-export declare class WrapLocalStorage implements Storage {
-    private _storage;
-    constructor(localStorage: any);
-    getItem(app: string): Array<LoggerageObject>;
-    setItem(app: string, value: LoggerageObject): void;
-    clear(): void;
-}
